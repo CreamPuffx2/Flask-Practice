1b8fd913c9318ec01d55b93ef1dbfa6cc04d4996
CreamPuffx2 <exploit017@gmail.com>
2020-10-07 22:40:22 +0900

    [MAKE] Minitor app.py (/sign-up)

---
+++ Miniter/app.py
@@ -0,0 +1,15 @@
+from flask import flask, jsonify, request
+
+app = flask(__name__)
+app.users = {}
+app.id_count = 1
+
+
+@app.route("/sign-up", methods=["POST"])
+def sign_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+
+    return jsonify(new_user)
